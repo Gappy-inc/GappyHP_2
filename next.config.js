3bdb19f7a2a8baf904e8015dacbf69c1fd00f304
先端情報領域プログラム <yuta0602yuta@gmail.com>
2025-11-29 14:42:57 +0900

    リダイレクトURL

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'gappy-hp-2.vercel.app',
+          },
+        ],
+        destination: 'https://official.gappy.jp/:path*',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
